8b1319c63a530c3addcf953c433522b0233582b5
cq230 <1665508818@qq.com>
2024-12-18 22:33:17 +0800

    Site updated: 2024-12-18 22:33:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4a17b156.html","posts/4d40bec4.html","posts/d449ef7e.html","posts/3a478e52.html","posts/a4231bf1.html","posts/d3242b67.html","posts/ba55eea9.html","posts/4a2d7add.html"];function toRandomPost(){
+var posts=["posts/d449ef7e.html","posts/4a17b156.html","posts/3a478e52.html","posts/4d40bec4.html","posts/a4231bf1.html","posts/d3242b67.html","posts/ba55eea9.html","posts/4a2d7add.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
