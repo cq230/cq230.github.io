4cbf9c23d9cf9b13f334b5ee93fa86d9e893da5c
cq230 <1665508818@qq.com>
2024-12-19 01:50:21 +0800

    Site updated: 2024-12-19 01:50:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/d449ef7e.html","posts/4a17b156.html","posts/4d40bec4.html","posts/3a478e52.html","posts/d3242b67.html","posts/a4231bf1.html","posts/ba55eea9.html","posts/4a2d7add.html","posts/cd52de3f.html"];function toRandomPost(){
+var posts=["posts/4a17b156.html","posts/4d40bec4.html","posts/d449ef7e.html","posts/3a478e52.html","posts/d3242b67.html","posts/4a2d7add.html","posts/cd52de3f.html","posts/ba55eea9.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
