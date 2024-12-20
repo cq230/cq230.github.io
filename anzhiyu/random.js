7517325737a2d2b9c0f77c346906cce42b9313fa
cq230 <1665508818@qq.com>
2024-12-20 18:55:41 +0800

    Site updated: 2024-12-20 18:55:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/9a542ee1.html"];function toRandomPost(){
+var posts=["posts/80c02825.html","posts/9a542ee1.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
